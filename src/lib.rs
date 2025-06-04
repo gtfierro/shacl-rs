fbe3c07179dacf47ed63b54399a1d2eec6c53e8f
Gabe Fierro <gtfierro@mines.edu>
2025-06-04 12:45:49 -0400

    feat: add initial src/lib.rs file with base implementation

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code, unused_variables)]
+mod types;
+mod components;
+mod shape;
+mod context;
+
+use components::Component;
+use shape::Shape;
+use types::ID;
+use std::collections::HashMap;
+
+pub struct Store {
+    shape_lookup: HashMap<ID, Shape>,
+    component_lookup: HashMap<ID, Component>,
+}
+
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
